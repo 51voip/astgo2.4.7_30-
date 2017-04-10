ead928dfbc3d16f19258d6c5588160e40e292750
51VOIP.ORG <2270992199@qq.com>
2017-04-11 00:21:30 +0800

    no commit message

---
+++ astgo_fist_run.sh
@@ -0,0 +1,37 @@
+﻿#!/bin/bash
+clear
+echo "Install ASTGO  Now!!!............."
+cd /tmp/
+/bin/tar -zxvf astgo2-current.tar.gz
+cd /tmp/astgo2/
+
+/bin/chmod +x update1.sh
+/bin/chmod +x install1.sh
+
+./update1.sh
+./install1.sh
+
+
+
+/bin/rm  /tmp/astgo_fist_run.sh
+/bin/rm  /tmp/astgo2/astgo_fist_run.sh
+
+
+
+
+
+
+
+echo " Install astgo Completed "
+sleep 10
+
+echo ""
+echo " ...            Thank you           ..."
+sleep 10
+
+reboot
+
+
+
+
+ 
